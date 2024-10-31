8749dcb26b2b4341130eff5513f4f5b2f280eece
Alexey <av.bakulin7@gmail.com>
2024-10-31 23:50:14 +0300

    Docker

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/healthz', methods=['GET'])
+def hello_get():
+    return '', 200
+
+@app.route('/healthz', methods=['POST'])
+def hello_post():
+    return '', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
